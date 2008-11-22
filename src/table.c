thiagoarrais/console

commit cfb8aa6ee2526c650057641df7959d50a4524451

2007-02-05  Chris Wilson  <chris@chris-wilson.co.uk>
	The continual reallocation of GValueArray is the last trivially
	avoidable g_realloc.

	* src/interpret.c: (main):
	* src/matcher.c: (_vte_matcher_create), (_vte_matcher_destroy),
	(_vte_matcher_match), (_vte_matcher_free_params_array):
	* src/matcher.h:
	* src/table.c: (_vte_table_extract_string):
	* src/trie.c: (_vte_trie_matchx), (_vte_trie_match), (main):
	* src/vte.c: (vte_terminal_process_incoming):



git-svn-id: svn+ssh://svn.gnome.org/svn/vte/trunk@1622 c1152ede-e825-0410-90e1-bb353d73734c

src/table.c
@@ -546,7 +546,7 @@ _vte_table_extract_numbers(GValueArray **array,
 			total *= 10;
 			total += v == -1 ?  0 : v;
 		}
-		if (*array == NULL) {
+		if (G_UNLIKELY (*array == NULL)) {
 			*array = g_value_array_new(1);
 		}
 		g_value_set_long(&value, total);
@@ -561,18 +561,17 @@ _vte_table_extract_string(GValueArray **array,
 {
 	GValue value = {0,};
 	gunichar *ptr;
-	int i;
+	guint i;
 
 	ptr = g_new(gunichar, arginfo->length + 1);
-	memcpy(ptr, arginfo->start, arginfo->length * sizeof(gunichar));
 	for (i = 0; i < arginfo->length; i++) {
-		ptr[i] &= ~(VTE_ISO2022_ENCODED_WIDTH_MASK);
+		ptr[i] = arginfo->start[i] & ~VTE_ISO2022_ENCODED_WIDTH_MASK;
 	}
-	ptr[arginfo->length] = '\0';
+	ptr[i] = '\0';
 	g_value_init(&value, G_TYPE_POINTER);
 	g_value_set_pointer(&value, ptr);
 
-	if (*array == NULL) {
+	if (G_UNLIKELY (*array == NULL)) {
 		*array = g_value_array_new(1);
 	}
 	g_value_array_append(*array, &value);
@@ -588,7 +587,7 @@ _vte_table_extract_char(GValueArray **array,
 	g_value_init(&value, G_TYPE_LONG);
 	g_value_set_long(&value, *(arginfo->start) - increment);
 
-	if (*array == NULL) {
+	if (G_UNLIKELY (*array == NULL)) {
 		*array = g_value_array_new(1);
 	}
 	g_value_array_append(*array, &value);

src/interpret.c
@@ -148,7 +148,7 @@ main(int argc, char **argv)
 			}
 		}
 		if (values != NULL) {
-			_vte_matcher_free_params_array(values);
+			_vte_matcher_free_params_array(matcher, values);
 		}
 		g_print(")\n");
 		i += l;

src/matcher.c
@@ -30,6 +30,7 @@
 struct _vte_matcher {
 	_vte_matcher_match_func match; /* shortcut to the most common op */
 	struct _vte_matcher_impl *impl;
+	GValueArray *free_params;
 };
 
 static GStaticMutex _vte_matcher_mutex = G_STATIC_MUTEX_INIT;
@@ -134,6 +135,7 @@ _vte_matcher_create(gpointer key)
 	ret = g_slice_new(struct _vte_matcher);
 	ret->impl = &dummy_vte_matcher_trie;
 	ret->match = NULL;
+	ret->free_params = NULL;
 
 	if (strcmp(emulation, "xterm") == 0) {
 		ret->impl = &dummy_vte_matcher_table;
@@ -152,6 +154,9 @@ _vte_matcher_destroy(gpointer value)
 	struct _vte_matcher *matcher = value;
 
 	_vte_debug_print(VTE_DEBUG_LIFECYCLE, "_vte_matcher_destroy()\n");
+	if (matcher->free_params != NULL) {
+		g_value_array_free (matcher->free_params);
+	}
 	if (matcher->match != NULL) /* do not call destroy on dummy values */
 		matcher->impl->klass->destroy(matcher->impl);
 	g_slice_free(struct _vte_matcher, matcher);
@@ -171,7 +176,7 @@ _vte_matcher_new(const char *emulation, struct _vte_termcap *termcap)
 	if (_vte_matcher_cache == NULL) {
 		_vte_matcher_cache = g_cache_new(_vte_matcher_create,
 				_vte_matcher_destroy,
-			       	(GCacheDupFunc) g_strdup, g_free,
+				(GCacheDupFunc) g_strdup, g_free,
 				g_str_hash, g_direct_hash, g_str_equal);
 	}
 
@@ -204,6 +209,10 @@ _vte_matcher_match(struct _vte_matcher *matcher,
 		   const char **res, const gunichar **consumed,
 		   GQuark *quark, GValueArray **array)
 {
+	if (matcher->free_params != NULL) {
+		*array = matcher->free_params;
+		matcher->free_params = NULL;
+	}
 	return matcher->match(matcher->impl, pattern, length,
 					res, consumed, quark, array);
 }
@@ -219,19 +228,22 @@ _vte_matcher_print(struct _vte_matcher *matcher)
  * themselves, but we're using gpointers to hold unicode character strings, and
  * we need to free those ourselves. */
 void
-_vte_matcher_free_params_array(GValueArray *params)
+_vte_matcher_free_params_array(struct _vte_matcher *matcher,
+		               GValueArray *params)
 {
 	guint i;
-	GValue *value;
-	if (params != NULL) {
-		for (i = 0; i < params->n_values; i++) {
-			value = g_value_array_get_nth(params, i);
-			if (G_VALUE_HOLDS_POINTER(value)) {
-				g_free(g_value_get_pointer(value));
-				g_value_set_pointer(value, NULL);
-			}
+	for (i = 0; i < params->n_values; i++) {
+		GValue *value = g_value_array_get_nth(params, i);
+		if (G_VALUE_HOLDS_POINTER(value)) {
+			g_free(g_value_get_pointer(value));
+			g_value_set_pointer(value, NULL);
 		}
+	}
+	if (G_UNLIKELY (matcher == NULL || matcher->free_params != NULL)) {
 		g_value_array_free(params);
+	} else {
+		matcher->free_params = params;
+		params->n_values = 0;
 	}
 }
 

src/matcher.h
@@ -69,7 +69,7 @@ const char *_vte_matcher_match(struct _vte_matcher *matcher,
 void _vte_matcher_print(struct _vte_matcher *matcher);
 
 /* Free a parameter array. */
-void _vte_matcher_free_params_array(GValueArray *params);
+void _vte_matcher_free_params_array(struct _vte_matcher *matcher, GValueArray *params);
 
 G_END_DECLS
 

src/trie.c
@@ -632,13 +632,15 @@ _vte_trie_matchx(struct _vte_trie *trie, const gunichar *pattern, gsize length,
 				if (better) {
 					best = tmp;
 					if (bestarray != NULL) {
-						_vte_matcher_free_params_array(bestarray);
+						_vte_matcher_free_params_array(
+								NULL, bestarray);
 					}
 					bestarray = tmparray;
 					bestquark = tmpquark;
 					bestconsumed = *consumed;
 				} else {
-					_vte_matcher_free_params_array(tmparray);
+					_vte_matcher_free_params_array(
+							NULL, tmparray);
 					tmparray = NULL;
 				}
 			}
@@ -650,12 +652,12 @@ _vte_trie_matchx(struct _vte_trie *trie, const gunichar *pattern, gsize length,
 		for (i = 0; i < bestarray->n_values; i++) {
 			GValue *value = g_value_array_get_nth(bestarray, i);
 			g_value_array_append(array, value);
-			
+
 			if (G_VALUE_HOLDS_POINTER(value)) {
 				g_value_set_pointer(value, NULL);
 			}
 		}
-		_vte_matcher_free_params_array(bestarray);
+		_vte_matcher_free_params_array(NULL, bestarray);
 	}
 #if 0
 	printf("`%s' ", best);
@@ -706,7 +708,7 @@ _vte_trie_match(struct _vte_trie *trie, const gunichar *pattern, gsize length,
 					g_value_set_pointer(value, NULL);
 				}
 			}
-			_vte_matcher_free_params_array(valuearray);
+			_vte_matcher_free_params_array(NULL, valuearray);
 		}
 		if (array != NULL) {
 			*array = NULL;
@@ -715,7 +717,7 @@ _vte_trie_match(struct _vte_trie *trie, const gunichar *pattern, gsize length,
 		if (array != NULL) {
 			*array = valuearray;
 		} else {
-			_vte_matcher_free_params_array(valuearray);
+			_vte_matcher_free_params_array(NULL, valuearray);
 		}
 	}
 
@@ -904,7 +906,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -916,7 +918,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -928,7 +930,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -940,7 +942,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -952,7 +954,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -964,7 +966,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -976,7 +978,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -988,7 +990,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -1000,7 +1002,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -1012,7 +1014,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -1024,7 +1026,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 	}
 
 	quark = 0;
@@ -1035,7 +1037,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 	}
 
 	quark = 0;
@@ -1046,7 +1048,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 	}
 
 	quark = 0;
@@ -1057,7 +1059,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 	}
 
 	quark = 0;
@@ -1068,7 +1070,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -1081,7 +1083,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 
@@ -1093,7 +1095,7 @@ main(int argc, char **argv)
 	printf("=> `%s' (%d)\n", g_quark_to_string(quark), (int)(consumed - buf));
 	if (array != NULL) {
 		dump_array(array);
-		_vte_matcher_free_params_array(array);
+		_vte_matcher_free_params_array(NULL, array);
 		array = NULL;
 	}
 

src/vte.c
@@ -3181,7 +3181,8 @@ vte_terminal_process_incoming(VteTerminal *terminal)
 
 		if (G_UNLIKELY(params != NULL)) {
 			/* Free any parameters we don't care about any more. */
-			_vte_matcher_free_params_array(params);
+			_vte_matcher_free_params_array(terminal->pvt->matcher,
+					params);
 			params = NULL;
 		}
 	}

commit 498a9569888aaab6789675dd65c3ff198ee21f15

* src/table.c: (print_array): Fix the build (broken since r1622).
git-svn-id: svn+ssh://svn.gnome.org/svn/vte/trunk@2175 c1152ede-e825-0410-90e1-bb353d73734c

src/table.c
@@ -856,7 +856,7 @@ print_array(GValueArray *array)
 			}
 		}
 		printf(")");
-		_vte_matcher_free_params_array(array);
+		/* _vte_matcher_free_params_array(array); */
 	}
 }
 