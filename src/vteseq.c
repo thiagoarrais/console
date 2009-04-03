1e0f18ee01b89cd8fa977ddcf04392151dfaa1c7
Thiago Arrais <thiago.arrais@gmail.com>
2009-04-03 10:45:30 -0300

    Always allow cursor to backwrap, since we're always autowrapping

--- src/vteseq.c
+++ src/vteseq.c
@@ -1763,21 +1763,17 @@ vte_sequence_handler_le (VteTerminal *terminal, GValueArray *params)
 		/* There's room to move left, so do so. */
 		screen->cursor_current.col--;
 		_vte_terminal_cleanup_tab_fragments_at_cursor (terminal);
-    vte_terminal_cursor_left(terminal);
-	} else {
-		if (terminal->pvt->flags.bw) {
-			/* Wrap to the previous line. */
-			screen->cursor_current.col = terminal->column_count - 1;
-			if (screen->scrolling_restricted) {
-				vte_sequence_handler_sr (terminal, params);
-			} else {
-				screen->cursor_current.row = MAX(screen->cursor_current.row - 1,
-								 screen->insert_delta);
-			}
+		vte_terminal_cursor_left(terminal);
+	} else if (screen->cursor_current.row > 0) {
+		/* Wrap to the previous line. */
+		screen->cursor_current.col = terminal->column_count - 1;
+		if (screen->scrolling_restricted) {
+			vte_sequence_handler_sr (terminal, params);
 		} else {
-			/* Stick to the first column. */
-			screen->cursor_current.col = 0;
+			screen->cursor_current.row = MAX(screen->cursor_current.row - 1,
+							 screen->insert_delta);
 		}
+		vte_terminal_cursor_left(terminal);
 	}
 }
 
